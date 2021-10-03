c5080caf883e2a1cb7abee06e582d69c33439196
devgg-source <ggkarthikmessi007@gmail.com>
2021-10-03 18:26:45 +0530

    Functional component created

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalComponent() {
+//   return <h1>Hello Karthik</h1>;
+// }
+
+export const FunctionalComponent = () => <h1>Hello Karthik</h1>;
+
+// export default FunctionalComponent;
